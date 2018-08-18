122cc516afc67788b7308bb9dda08d5237141017
Jugal <jugaldeepak@gmail.com>
2018-08-18 20:20:24 +0530

    File upload

---
+++ Programs/Binary Search.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+void Sort(int A[],int n)
+{
+	int i,j,min,temp;
+	for(i=0;i<n;i++)
+	{
+		min=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(A[j]<A[min])
+				min=j;
+		}
+		temp=A[i];
+		A[i]=A[min];
+		A[min]=temp;
+	}
+}
+int Search(int A[],int low,int high,int key)
+{
+	int mid,i,j;
+	mid=(low+high)/2;
+	if(A[mid]==key)
+		return mid+1;
+	else if(A[mid]>key)
+		return Search(A,low,mid-1,key);
+	else if(A[mid]<key)
+		return Search(A,mid+1,high,key);
+	else
+		return -1;
+}
+int main()
+{
+	int A[100],i,j,n,pos,key;
+	printf("enter number of elements\n");
+	scanf("%d",&n);
+	printf("enter array elements\n");
+	for(i=0;i<n;i++)
+		scanf("%d",&A[i]);
+	printf("\nenter the element to be searched for\n");
+	scanf("%d",&key);
+	Sort(A,n);
+	printf("\nSorted array is\n");
+	for(i=0;i<n;i++)
+		printf("%d\t",A[i]);
+	printf("\n");
+	pos=Search(A,0,n-1,key);
+	if (pos==-1)
+		printf("element not found\n");
+	else
+		printf("element found at %d\n",pos);
+}
